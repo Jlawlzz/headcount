5f1dcbdeda0b21aa993ce7776da0a58735841d1d
Steve Pentler <stevepentler@gmail.com>
2015-11-10 13:45:42 -0700

    edited sample file directory, need to figure out csv.readlines

--- test/enrollment_formatter_test.rb
+++ test/enrollment_formatter_test.rb
@@ -6,25 +6,25 @@ require './lib/enrollment_formatter'
 class EnrollmentFormatterTest < Minitest::Test
 
   def test_handle_equals_correct_file_path
-    ef = EnrollmentFormatter.new("/Users/JordanLawler/turing/1module/projects/head-count/headcount/data/kindergartners dummy .csv")
+    ef = EnrollmentFormatter.new("./data/kindergartners dummy .csv")
     assert_equal CSV, ef.handle.class
   end
 
   def test_creates_sub_hash_year_percent_for_single_line
-    ef = EnrollmentFormatter.new("/Users/JordanLawler/turing/1module/projects/head-count/headcount/data/kindergartners dummy .csv")
+    ef = EnrollmentFormatter.new("./data/kindergartners dummy .csv")
     hash = {2007 => 0.39465}
     assert_equal hash, ef.pair_year_percentage({:location => "Colorado", :TimeFrame => 2007, :DataFormat => "Percent", :Data => 0.39465})
   end
 
   def test_pairs_name_and_pair_for_single_line
-      ef = EnrollmentFormatter.new("/Users/JordanLawler/turing/1module/projects/head-count/headcount/data/kindergartners dummy .csv")
+    ef = EnrollmentFormatter.new("./data/kindergartners dummy .csv")
       hash = {:name => "district", :kindergarten_participation => {2010 => 0.3915}}
       csv_line = {:location => "district", :TimeFrame => 2010, :DataFormat => "Percent", :Data => 0.3915}
       assert_equal hash, ef.pair_name_and_year_percentage(csv_line)
   end
 
   def test_pairs_name_and_pair_for_repeat_district_name
-    ef = EnrollmentFormatter.new("/Users/JordanLawler/turing/1module/projects/head-count/headcount/data/kindergartners dummy .csv")
+    ef = EnrollmentFormatter.new("./data/kindergartners dummy .csv")
     hash = {:name => "district", :kindergarten_participation => {2009 => 0.1}}
     csv_line = {:location => "district", :TimeFrame => 2010, :DataFormat => "Percent", :Data => 0.3915}
     hash_mod = {:name => "district", :kindergarten_participation => {2009 => 0.1, 2010 => 0.3915}}
@@ -32,14 +32,14 @@ class EnrollmentFormatterTest < Minitest::Test
   end
 
   def test_single_line_format_for_unique_district_name
-    ef = EnrollmentFormatter.new("/Users/JordanLawler/turing/1module/projects/head-count/headcount/data/kindergartners dummy .csv")
+    ef = EnrollmentFormatter.new("./data/kindergartners dummy .csv")
     hash = {:name => "Colorado", :kindergarten_participation => {2007 => 0.39465}}
     ef.single_line_format({:location => "Colorado", :TimeFrame => 2007, :DataFormat => "Percent", :Data => 0.39465})
     assert_equal hash, ef.hash_bin[0]
   end
 
   def test_single_line_format_for_duplicate_district_name
-    ef = EnrollmentFormatter.new("/Users/JordanLawler/turing/1module/projects/head-count/headcount/data/kindergartners dummy .csv")
+    ef = EnrollmentFormatter.new("./data/kindergartners dummy .csv")
     ef.hash_bin << {:name => "Colorado", :kindergarten_participation => {2009 => 0.1}}
     hash_mod = [{:name => "Colorado", :kindergarten_participation => {2009 => 0.1, 2010 => 0.3915}}]
     assert_equal hash_mod, ef.single_line_format({:location => "Colorado", :TimeFrame => 2010, :DataFormat => "Percent", :Data => 0.3915})
