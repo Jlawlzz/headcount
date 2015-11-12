a9598927dc72aaa5a6c35fb25145b72254da404e
Steve Pentler <stevepentler@gmail.com>
2015-11-11 19:23:34 -0700

    added kindergarten participation rate variation and tests to headcount_analyst files

--- lib/headcount_analyst.rb
+++ lib/headcount_analyst.rb
@@ -13,10 +13,12 @@ class HeadcountAnalyst
     district2_avg = district2.enrollment.participation_years.values.inject(:+) / district2.enrollment.participation_years.count
   end 
 
-  # def kindergarten_participation_rate_variation(district1, district2)
-  #   district1_avg
-  # #disrict average (of all years) / :against district average(of all years)
-  # end
+  def kindergarten_participation_rate_variation(district1, district2)
+    comparison = district1_average(district1, district2) / district2_average(district1, district2)
+    comparison.round(3)
+  end
+
+  
 end 
 
  
